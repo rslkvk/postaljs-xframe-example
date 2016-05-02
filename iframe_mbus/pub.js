69ab7d91bc0fe63a47eca51ea0b44e7eac12713f
Kavakli <resul.kavakli@capgemini.com>
2016-05-02 14:17:58 +0200

    iFrame-MessageBus library implementation to simple use

--- iframe_mbus/pub.js
+++ iframe_mbus/pub.js
@@ -1,77 +1,62 @@
 // optional: you can add a topic as third parameter.
-var publish = (function (pId, pChannel, fChannel) {
-    //optional part of initializing
-    var topic = "#";
-    if (arguments.length == 4) {
-        topic = arguments[3];
+var pub = (function() {
+    var parentId, parentChannel, topic;
+    function init(pId, pChannel) { 
+        //optional part of initializing
+        topic = "#";
+        if (arguments.length == 4) {
+            topic = arguments[3];
+        }
+        parentId = pId, parentChannel = pChannel;
+
+        postal.instanceId(pId);
+        console.log("publisher \"" + pId + "\"" + " is initialized...");
+
+        /*
+         ||---------------------- From the PostalJS Documentation -----------------------||
+         || You can optionally configure postal.xframe with the configure call.          ||
+         || `allowedOrigins` is an array of origins that you can use to determine        ||
+         || if you want to federate with postal instances loaded in another window       ||
+         || from those origins.  If another host attempts to federate with you from      ||
+         || an origin not listed in that array, the local instance of postal will        ||
+         || not allow it. The local instance of postal will not send any messages to     ||
+         || (nor process any from) an origin not listed in this array.                   ||
+         || `defaultOriginUrl` is the default value that will be provided as the         ||
+         || "targetUrl" when postal.xframe calls `window.postMessage(msg, targetUrl)`    ||
+         || if it hasn't been specified for that remote window already.                  ||
+         ||     postal.fedx.transports.xframe.configure({                                ||
+         ||         allowedOrigins : [ "http://some.host.com", "http://another.com" ],   ||
+         ||         defaultOriginUrl : "http://some.host.com",                           ||
+         ||         enabled: true // this is redundant - just showing that it's here     ||
+         ||     });                                                                      ||
+         ||------------------------------------------------------------------------------||
+         */
+        // As we use the reverse proxy we can skip this step
+        // postal.fedx.transports.xframe.configure({
+        //     allowOrigins: ["./index.html", ""],
+        //     defaultOriginUrl: ["./index.html"]
+        // })
+
+        postal.fedx.addFilter([
+            {channel: pChannel, topic: "#", direction: "out"}
+        ]);
+
+        postal.fedx.signalReady();
+
+
+        console.log("publisher \"" + pId + "\"" + " is ready on" +
+            " channel \"" + pChannel + "\"...");
     }
 
-    postal.instanceId(pId);
-    console.log("parent \"" + pId + "\"" + "is initialized...");
-
-    /*
-     ||---------------------- From the PostalJS Documentation -----------------------||
-     || You can optionally configure postal.xframe with the configure call.          ||
-     || `allowedOrigins` is an array of origins that you can use to determine        ||
-     || if you want to federate with postal instances loaded in another window       ||
-     || from those origins.  If another host attempts to federate with you from      ||
-     || an origin not listed in that array, the local instance of postal will        ||
-     || not allow it. The local instance of postal will not send any messages to     ||
-     || (nor process any from) an origin not listed in this array.                   ||
-     || `defaultOriginUrl` is the default value that will be provided as the         ||
-     || "targetUrl" when postal.xframe calls `window.postMessage(msg, targetUrl)`    ||
-     || if it hasn't been specified for that remote window already.                  ||
-     ||     postal.fedx.transports.xframe.configure({                                ||
-     ||         allowedOrigins : [ "http://some.host.com", "http://another.com" ],   ||
-     ||         defaultOriginUrl : "http://some.host.com",                           ||
-     ||         enabled: true // this is redundant - just showing that it's here     ||
-     ||     });                                                                      ||
-     ||------------------------------------------------------------------------------||
-     */
-    // As we use the reverse proxy we can skip this step
-    // postal.fedx.transports.xframe.configure({
-    //     allowOrigins: ["./index.html", ""],
-    //     defaultOriginUrl: ["./index.html"]
-    // })
-
-    postal.fedx.addFilter([
-        {channel: pChannel, topic: "#", direction: "out"}
-    ]);
-
-    postal.fedx.signalReady();
-
-
-    console.log("parent \"" + pId + "\"" + " is ready on" +
-        " channel \"" + pChannel + "\"...");
-
-    return (function(pChannel) {
-        return function (data) {
-            console.log("publishing from " + pId + " " +
-                "on channel " + pChannel + "...");
-            postal.publish({
-                channel: pChannel,
-                topic: topic,
-                data: data
-            });
-            console.log("... " + JSON.stringify(data));
-            return false;
-        };
-    });
-
-    var subscribe = (function(fChannel) {
-        postal.fedx.addFilter({channel:fChannel, topic: '#', direction: "in"});
-
-        return function() { return postal.subscribe({
-            channel: fChannel,
+    function publish(data) {
+        console.log("publishing from " + parentId + " on channel " + parentChannel + "...");
+        postal.publish({
+            channel: parentChannel,
             topic: topic,
-            callback: function (data, envelope) {
-                console.log("subscption gets this message: " + JSON.stringify(data) + "...");
-                return data;
-            }
-        });};
-    }(fChannel));
-
-}("portal", "output", "input"));
-
-
-
+            data: data
+        });
+        console.log("... " + JSON.stringify(data));
+        return false;
+    }
+    return {init:init, publish:publish};
+}());
--- iframe_mbus/sub.js
+++ iframe_mbus/sub.js
@@ -1,61 +1,38 @@
 // optional: you can add a topic as third parameter.
-var subscribe = (function (subId, channel) {
-    //optional part of initializing
-    var topic = "#";
-    if(arguments.length == 3) {
-        topic = arguments[2];
+var sub = (function () {
+
+    var frameId, frameChannel, topic;
+
+
+    function init(subId, subChannel) {
+        //optional part of initializing
+        topic = "#";
+        if (arguments.length == 3) {
+            topic = arguments[2];
+        }
+        frameId = subId;
+        frameChannel = subChannel;
+        
+        postal.instanceId(frameId);
+        console.log("subscriber \"" + frameId + "\"" + " is initialized...");
+        postal.fedx.addFilter([
+            {channel: frameChannel, topic: topic, direction: "in"}
+        ]);
+        postal.fedx.signalReady();
+        console.log("subscriber \"" + frameId + "\"" + " is ready on" +
+            " channel \"" + frameChannel + "\"...");
     }
 
-    postal.instanceId(pubId);
-    console.log("subscriber <"+subId+">" + "is initialized...");
-
-    /*
-     ||---------------------- From the PostalJS Documentation -----------------------||
-     || You can optionally configure postal.xframe with the configure call.          ||
-     || `allowedOrigins` is an array of origins that you can use to determine        ||
-     || if you want to federate with postal instances loaded in another window       ||
-     || from those origins.  If another host attempts to federate with you from      ||
-     || an origin not listed in that array, the local instance of postal will        ||
-     || not allow it. The local instance of postal will not send any messages to     ||
-     || (nor process any from) an origin not listed in this array.                   ||
-     || `defaultOriginUrl` is the default value that will be provided as the         ||
-     || "targetUrl" when postal.xframe calls `window.postMessage(msg, targetUrl)`    ||
-     || if it hasn't been specified for that remote window already.                  ||
-     ||     postal.fedx.transports.xframe.configure({                                ||
-     ||         allowedOrigins : [ "http://some.host.com", "http://another.com" ],   ||
-     ||         defaultOriginUrl : "http://some.host.com",                           ||
-     ||         enabled: true // this is redundant - just showing that it's here     ||
-     ||     });                                                                      ||
-     ||------------------------------------------------------------------------------||
-     */
-    // As we use the reverse proxy we can skip this step
-    // postal.fedx.transports.xframe.configure({
-    //     allowOrigins: ["./index.html", ""],
-    //     defaultOriginUrl: ["./index.html"]
-    // })
-
-    postal.fedx.addFilter([
-        {channel: channel, topic: "#", direction: "in"}
-    ]);
-
-    postal.fedx.signalReady();
-
-
-    console.log("subscriber \""+subId+"\"" + " is ready on" +
-        " channel \"" + channel+"\"...");
-
-    return function(data) {
-        console.log("publishing from " + pubId +" " +
-            "on channel " + channel + "..." );
-
-        postal.publish({
-            channel: channel,
+    function subscribe() {
+        var sd = postal.subscribe({
+            channel: frameChannel,
             topic: topic,
-            data: data
-        })  ;
+            callback: function (data, envelope) {
+                console.log("subscription gets this message: " + JSON.stringify(data) + "...");
+                return data;
+            }});
+        return sd;
+    }
 
-        console.log("... " + JSON.stringify(data));
-        return false;
-    };
-
-} ("parent", "frame"));
+    return {init:init, subscribe:subscribe};
+}());
